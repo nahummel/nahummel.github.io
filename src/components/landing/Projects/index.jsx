7ea7e4a8452168cc906b29bf97196001a9dbaf1e
nahummel <natoms225@gmail.com>
2020-07-18 16:55:00 -0500

    Initial Commit

---
+++ src/components/landing/Projects/index.jsx
@@ -0,0 +1,16 @@
+import React, { useContext } from 'react';
+import { ThemeContext } from 'providers/ThemeProvider';
+import { Container } from 'components/common';
+import { Wrapper, Grid } from './styles';
+
+export const Projects = () => {
+  
+  return (
+    <Wrapper as={Container} id="projects">
+      <h2>Projects</h2>
+      <Grid>
+        
+      </Grid>
+    </Wrapper>
+  );
+};
